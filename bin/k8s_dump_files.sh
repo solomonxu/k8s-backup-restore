f2a7e7069296cacfd8289985f996f336769d5476
solomonxu <solomonxu@163.com>
2019-10-23 10:09:59 +0800

    Add shell of dump files from K8s containers

--- bin/k8s_dump_files.sh
+++ bin/k8s_dump_files.sh
@@ -4,6 +4,9 @@
 echo "usage: $0 [configfile]"
 echo "	configfile: the default config file is podfile.conf"
 
+## define constants
+CMD_LIST="ps.-ef ps.-efL lsof.-a"
+
 ## define variable
 BACKUP_PATH=/data/k8s-backup-restore
 BACKUP_PATH_BIN=${BACKUP_PATH}/bin
@@ -18,23 +21,20 @@ LOG_OUTPUT_FILE_DEFAULT="${LOG_PATH}/backup.log"
 ## Include shells
 . ${BACKUP_PATH}/bin/logger.sh
 
-## make dir
-mkdir -p ${BACKUP_PATH_BIN}
-mkdir -p ${BACKUP_PATH_DATA}
-mkdir -p ${LOG_PATH}
-cd ${BACKUP_PATH_DATA}
-
-## set config file
-_config_file=${BACKUP_PATH}/conf/podfile.conf
-if [ $# -ge 1 ]; then
-	_config_file=${BACKUP_PATH}/conf/$1
-fi
-
-## define counters
-_count=0
-## Dump container's files 
-function dump_container_files()
+## Initialize
+function init()
 {
+    ## make dir
+    mkdir -p ${BACKUP_PATH_BIN}
+    mkdir -p ${BACKUP_PATH_DATA}
+    mkdir -p ${LOG_PATH}
+    cd ${BACKUP_PATH_DATA}
+
+    ## set config file
+    _config_file=${BACKUP_PATH}/conf/podfile.conf
+    if [ $# -ge 1 ]; then
+    	_config_file=${BACKUP_PATH}/conf/$1
+    fi
     ## check pod config file
     if [ ! -f "${_config_file}" ]; then
         logger_error "Pod config ${_config_file} not existed, exit."
@@ -43,7 +43,14 @@ function dump_container_files()
     ## read pod config file
     _podfile_list=$(cat ${_config_file} | grep -v "##" | awk 'BEGIN{OFS="#"}{if($0!=""){print $1,$2,$3,$4}}')
 	logger_info "_podfile_list=${_podfile_list}"
+}
+
+## Dump container's files 
+function dump_container_files()
+{
 	_namespace_prior=""
+	## define counters
+	_count=0
     ## loop for podfile list
     for _podfile_line in ${_podfile_list}; do
         ## check podfile line
@@ -54,9 +61,8 @@ function dump_container_files()
         _namespace=$( echo ${_podfile_line} | awk -F# '{print $1}')
         _pod_prefix=$(echo ${_podfile_line} | awk -F# '{print $2}')
         _container=$( echo ${_podfile_line} | awk -F# '{print $3}')
-        _file_item=$( echo ${_podfile_line} | awk -F# '{print $4}')
-        _file_list=$( echo ${_file_item} | awk -F: '{print $1}')
-        logger_info "_namespace=${_namespace}, _pod_prefix=${_pod_prefix}, _container=${_container}, _file_list=${_file_list}"
+        _file_list=$( echo ${_podfile_line} | awk -F# '{print $4}' | awk -F: '{for(i=1;i<=NF;i++) print $i}')
+        logger_info "Config line: _namespace=${_namespace}, _pod_prefix=${_pod_prefix}, _container=${_container}, _file_list=${_file_list}"
         ## check pod argument
         if [ -z "${_pod_prefix}" ]; then
             continue;
@@ -81,8 +87,32 @@ function dump_container_files()
         ## get pod list
         _pod_list=$(kubectl get pods ${_ns_arg} | grep -v "NAME" | grep "${_pod_prefix}-" | awk '{print $1}')
         for _pod in ${_pod_list}; do
-            _pod_root=${BACKUP_PATH_DATA}/"${_namespace}_pod_${_pod}_${_container_suffix}"
+            _pod_root="${BACKUP_PATH_DATA}/${_namespace}_pod_${_pod}_${_container_suffix}"
             mkdir -p ${_pod_root}
+
+            ## Describe pod
+            _descfile="${_pod_root}/pod_desc.txt"
+            logger_info "Describe pod ${_pod} to file: ${_descfile}."
+            _ret_val=$(kubectl ${_ns_arg} describe pod ${_pod} > ${_descfile})
+            logger_info "Describe pod ${_pod} ok."
+
+            ## loop for command list
+            for _cmd in ${CMD_LIST}; do
+                _cmd=${_cmd//./ }
+                _cmd_output="${_pod_root}/cmd_output_${_container_suffix}.txt"
+                ## Execute command from container to local host
+                logger_info "Execute command '${_cmd}' at pod ${_pod} container ${_container}, output to file: ${_cmd_output}."
+                _ret_val=$(echo "# $(date '+%Y-%m-%d %H:%M:%S') Execute command: ${_cmd}" >> ${_cmd_output})
+                _ret_val=$(kubectl ${_ns_arg} exec -it ${_pod} ${_container_arg} -- ${_cmd} >> ${_cmd_output})
+                logger_info "Execute command ok."
+            done
+
+            ## Dump container log
+            _logfile="${_pod_root}/container_${_container_suffix}.log"
+            logger_info "Dump pod ${_pod} container ${_container_suffix} log to file: ${_logfile}."
+            _ret_val=$(kubectl ${_ns_arg} logs ${_pod} ${_container_arg} > ${_logfile})
+            logger_info "Dump pod ${_pod} log file ok."
+
             ## loop for file list
             for _file in ${_file_list}; do
                 _local_file="${_pod_root}${_file}"
@@ -91,7 +121,7 @@ function dump_container_files()
                 ## dump file/dir from container to local host
                 logger_info "Dump pod ${_pod} container ${_container} file ${_file} to local host: ${_local_file}."
                 _ret_val=$(kubectl ${_ns_arg} cp ${_pod}:${_file} ${_container_arg} ${_local_file})
-                logger_info "Dump pod ${_pod} file done."
+                logger_info "Dump pod ${_pod} file ok."
                 _count=$[_count+1]
             done
         done
@@ -103,13 +133,17 @@ function dump_container_files()
 function package_zip()
 {
     ## package container files to a zip file
-    ZIP_FILE=${BACKUP_PATH_PODFILE}/podfiles_${SUB_PATH}.tar
+    _config_affix=$( echo ${_config_file} | awk -F/ '{print $NF}' | awk -F. '{print $1}')
+    _zip_file="${BACKUP_PATH_PODFILE}/podfiles_${_config_affix}_${SUB_PATH}.tar"
     logger_info "Package container files into zip file..."
     cd ${BACKUP_PATH_PODFILE}
-    tar -cvPf ${ZIP_FILE} ${SUB_PATH}
-    gzip ${ZIP_FILE}
-    logger_info "Package container files into zip file: ${ZIP_FILE}.gz done."
+    tar -cvPf ${_zip_file} ${SUB_PATH}
+    gzip ${_zip_file}
+    logger_info "Package container files into zip file: ${_zip_file}.gz done."
 }
+
+## Initialize
+init $@;
 
 ## Dump container's files 
 dump_container_files;
